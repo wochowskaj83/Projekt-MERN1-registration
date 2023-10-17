5f6b56aa555ee00100923a62999f288023b8bcc6
wochowskaj83 <wochowskaj@gmail.com>
2023-10-17 10:01:09 +0200

    konfiguracja MongoDB i połaczenie z Bazą Danych

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const config = require('./config')
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const mongoUrl = `mongodb://${config.db.host}:${config.db.port}/${config.db.name}`
+
+mongoose
+.connect(mongoUrl, {})
+.then(()=> {
+    console.log('MongoDB is connected')
+})
+.catch((err)=> {
+    throw err
+})
 
 const app = express()
 app.use(express.json())
--- config.js
+++ config.js
@@ -1,5 +1,12 @@
 module.exports = {
     app: {
         port: 3000
+    },
+
+    db: {
+        host: '127.0.0.1',
+        port: 27017,
+        name: 'ProjektMERN1-registration',
+        password: ''
     }
 }
